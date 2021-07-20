e96e8eaa61aeb7ad06b33d7d8b7185834b94f629
serotonyn <youyou1youcef@gmail.com>
2021-07-20 22:45:44 +0100

    Switch from untitled to file

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { spaceAfterImportStatement } from './functions';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'untitled', language: "typescriptreact" }, {
+    vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'file', language: "typescriptreact" }, {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] | undefined {
             const text = document.getText();
             return spaceAfterImportStatement(document, text)
